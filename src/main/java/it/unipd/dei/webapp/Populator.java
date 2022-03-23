629e428a2cee813b72a0f7306983cd9e14812e4c
Andrea <andrea.pasin99@gmail.com>
2022-03-23 20:49:59 +0100

    Added javafaker and Populator basic class.

---
+++ src/main/java/it/unipd/dei/webapp/Populator.java
@@ -0,0 +1,36 @@
+package it.unipd.dei.webapp;
+import com.github.javafaker.Faker;
+
+/** Class that can be used to populate the tables of the WebApp
+ * using the javafaker library
+*/
+public class Populator {
+
+    /** Populate tables using javafaker and the DAO classes
+     */
+    public static void main(String[] args){
+
+    }
+
+    //TODO: define appropriately the following methods
+
+    /** Create the tables
+     */
+    public void migrate(){
+
+    }
+
+    /** Populate the tables
+     */
+    public void populate(){
+
+    }
+
+    /** Delete data from the tables
+     */
+    public void delete(){
+
+    }
+
+    //TODO: methods to populate each table using javafaker
+}
