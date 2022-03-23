ccc71b570087afdae864c2c455fb855a8b24d6a8
Francesco Caldivezzi <francesco.caldivezzi@gmail.com>
2022-03-23 22:03:42 +0100

    Added some sql tables

--- src/main/database/create.sql
+++ src/main/database/create.sql
@@ -1,1 +1,68 @@
 DROP DATABASE IF EXISTS gwa;
+
+
+//FINTO 
+CREATE TABLE IF NOT EXISTS passwordreset (
+	token BIGINT,
+	expirationdate TIMESTAMP NOT NULL,
+	user INTEGER,
+	PRIMARY KEY(token),
+	FOREIGN KEY(user) REFERENCES user(id)
+);
+
+//ALMOST OK
+CREATE TABLE IF NOT EXISTS user (
+	id SERIAL,
+	role roles NOT NULL,
+	name TEXT NOT NULL,
+	surname TEXT NOT NULL,
+	email TEXT NOT NULL,
+	psw BIGINT NOT NULL,
+	taxcode CHAR(16) UNIQUE NOT NULL,
+	birthdate DATE NOT NULL,
+	telephone ?? STABILIRE +39??,
+	address TEXT NOT NULL,
+	avatarpath TEXT,
+	PRIMARY KEY(id)
+);
+
+CREATE TYPE IF NOT EXISTS roles AS ENUM ('Trainee', 'Trainer', 'Secretary');
+
+
+//FINITO
+CREATE TABLE IF NOT EXISTS medicalcertificate (
+	user BIGINT,
+	expirationdate DATE,
+	doctorname TEXT NOT NULL,
+	path TEXT NOT NULL,
+	PRIMARY KEY(user,expirationdate),
+	FOREIGN KEY(user) REFERENCES user(id)
+);
+
+//FINITO
+CREATE TABLE IF NOT EXISTS reservation (
+	trainee INTEGER,
+	lectureroom TEXT,
+	lecturedate DATE,
+	lecturestarttime TIME
+	PRIMARY KEY(trainee,lectureroom,lecturedate,lecturestarttime),
+	FOREIGN KEY(trainee) REFERENCES user(id),
+	FOREIGN KEY(lectureroom,lecturedate,lecturestarttime) REFERENCES lecturetimeslot(roomname,date,starttime)
+);
+
+//OK
+CREATE TABLE IF NOT EXISTS teaches (
+	courseeditionid INTEGER,
+	coursename TEXT,
+	trainer INTEGER,
+	PRIMARY KEY(courseeditionid,coursename,trainer),
+	FOREIGN KEY(courseeditionid,coursename) REFERENCES courseedition(id,coursename)
+
+);
+
+//OK
+CREATE TABLE IF NOT EXISTS room (
+	name TEXT,
+	slots INTEGER NOT NULL,
+	PRIMARY KEY(name)
+);
