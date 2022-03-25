cfaec565a9f112f69537cc4e078f5276fe99f4f9
Francesco Caldivezzi <francesco.caldivezzi@gmail.com>
2022-03-25 16:24:39 +0100

    added Teaches DAO

---
+++ src/main/java/resource/Teaches.java
@@ -0,0 +1,41 @@
+package resource;
+
+public class Teaches
+{
+    private int courseEdition;
+    private String courseName;
+    private String trainer;
+
+    public Teaches(int courseEdition, String courseName, String trainer)
+    {
+        this.courseEdition = courseEdition;
+        this.courseName = courseName;
+        this.trainer = trainer;
+    }
+    public void setCourseEdition(int courseEdition)
+    {
+        this.courseEdition = courseEdition;
+    }
+    public void  setCourseName(String courseName)
+    {
+        this.courseName = courseName;
+    }
+    public void  setTrainer(String trainer)
+    {
+        this.trainer = trainer;
+    }
+    public int getCourseEdition()
+    {
+        return courseEdition;
+    }
+    public String getCourseName()
+    {
+        return  courseName;
+    }
+    public String getTrainer()
+    {
+        return trainer;
+    }
+
+
+}
