ad3d9029a3905af440e55b92c64c8a105eb385e9
Riccardo Forzan <riccardo.forzan@studenti.unipd.it>
2022-03-26 11:49:39 +0100

    Added courses and course editions

--- src/main/database/populate.sql
+++ src/main/database/populate.sql
@@ -1,35 +1,30 @@
---User
---MedialCertificate
---Teaches
---CourseEdition
---Course
-
 --Users
 INSERT INTO person VALUES ('mario.rossi@example.com',ARRAY['Trainee']::roles[],'Mario','Rossi','d41d8cd98f00b204e9800998ecf8427e','RSSMRO0000000000',TO_DATE('01/01/1970','DD/MM/YYYY'),'3000000001','Address of Mario Rossi',null);
 INSERT INTO person VALUES ('riccardo.bianchi@example.com',ARRAY['Trainee']::roles[],'Riccardo','Bianchi','d41d8cd98f00b204e9800998ecf8427e','RCRBNC0000000000',TO_DATE('01/02/1970','DD/MM/YYYY'),'3000000002','Address of Riccardo Bianchi',null);
 INSERT INTO person VALUES ('luca.giallo@example.com',ARRAY['Trainee']::roles[],'Luca','Giallo','d41d8cd98f00b204e9800998ecf8427e','LCLGLL0000000000',TO_DATE('01/03/1970','DD/MM/YYYY'),'3000000003','Address of Luca Giallo',null);
-INSERT INTO person VALUES ('giacomo.forza@example.com',ARRAY['Trainer']::roles[],'Giacomo','Forza','d41d8cd98f00b204e9800998ecf8427e','GCMFRZ0000000000',TO_DATE('01/06/1980','DD/MM/YYYY'),'3000000004','Address of Giacomo Forza',null);
 INSERT INTO person VALUES ('stanley.corkery@example.com',ARRAY['Trainee']::roles[],'Stanley','Corkery','d41d8cd98f00b204e9800998ecf8427e','GARCDBJVHEVHIILY',TO_DATE('21/02/1982','DD/MM/YYYY'),'8524881756','304 Jason Mills, Gradymouth, OK 59441',NULL);
 INSERT INTO person VALUES ('virginia.barrows@example.com',ARRAY['Trainee']::roles[],'Virginia','Barrows','d41d8cd98f00b204e9800998ecf8427e','PHDMJRPQFLCGNXVF',TO_DATE('26/01/1988','DD/MM/YYYY'),'0195723168','7610 Kling Drive, Port Nealborough, MA 60511-1715',NULL);
 INSERT INTO person VALUES ('sondra.dubuque@example.com',ARRAY['Trainee']::roles[],'Sondra','DuBuque','d41d8cd98f00b204e9800998ecf8427e','E4FHUQGI8YSMXC9O',TO_DATE('18/03/1999','DD/MM/YYYY'),'5280680379','Apt. 686 90669 Schroeder Center, Vivienfort, ID 47685',NULL);
+INSERT INTO person VALUES ('bart.collins@example.com',ARRAY['Trainee']::roles[],'Bart','Collins','d41d8cd98f00b204e9800998ecf8427e','YOY6TKBDKLICSDBB',TO_DATE('20/11/1993','DD/MM/YYYY'),'9849796961','61356 Erin Harbor, Lake Sherman, IL 47716',NULL);
+INSERT INTO person VALUES ('cristi.schaden@example.com',ARRAY['Trainee']::roles[],'Cristi','Schaden','d41d8cd98f00b204e9800998ecf8427e','DI2RTBAM8YWBSI5R',TO_DATE('14/06/1993','DD/MM/YYYY'),'8696726751','0149 Goyette Ports, West Crystalhaven, MN 53493',NULL);
+INSERT INTO person VALUES ('alonzo.lindgren@example.com',ARRAY['Trainee']::roles[],'Alonzo','Lindgren','d41d8cd98f00b204e9800998ecf8427e','MQRCLW9GUXOEOCZE',TO_DATE('02/08/1987','DD/MM/YYYY'),'0537593185','3249 Tanna Motorway, West Marlena, AK 22530',NULL);
+INSERT INTO person VALUES ('bao.glover@example.com',ARRAY['Trainee']::roles[],'Bao','Glover','d41d8cd98f00b204e9800998ecf8427e','W3CT5DQZUF3OIO2E',TO_DATE('03/12/1964','DD/MM/YYYY'),'6742914291','Suite 500 383 Myung Isle, West Hazelfort, KS 54896-0334',NULL);
+INSERT INTO person VALUES ('altha.ruecker@example.com',ARRAY['Trainee']::roles[],'Altha','Ruecker','d41d8cd98f00b204e9800998ecf8427e','7IM7ZA79M0JKEDOE',TO_DATE('27/09/1959','DD/MM/YYYY'),'7216835609','34074 Luciano Loaf, Timton, IN 24432-9964',NULL);
+INSERT INTO person VALUES ('giacomo.forza@example.com',ARRAY['Trainer']::roles[],'Giacomo','Forza','d41d8cd98f00b204e9800998ecf8427e','GCMFRZ0000000000',TO_DATE('01/06/1980','DD/MM/YYYY'),'3000000004','Address of Giacomo Forza',null);
+INSERT INTO person VALUES ('shery.stamm@example.com',ARRAY['Trainer']::roles[],'Shery','Stamm','d41d8cd98f00b204e9800998ecf8427e','WTUZV4V1DE1UFFUI',TO_DATE('17/10/1970','DD/MM/YYYY'),'6161813924','86778 Bogisich Road, Zoeview, AK 87873-9690',NULL);
+INSERT INTO person VALUES ('nathanial.prohaska@example.com',ARRAY['Trainer']::roles[],'Nathanial','Prohaska','d41d8cd98f00b204e9800998ecf8427e','RE1BE14AWWYX2Y4L',TO_DATE('06/01/1961','DD/MM/YYYY'),'1396809182','Suite 023 30803 Nader Valleys, Sengerville, PA 07282-1307',NULL);
 INSERT INTO person VALUES ('annis.dach@example.com',ARRAY['Trainer']::roles[],'Annis','Dach','d41d8cd98f00b204e9800998ecf8427e','IPYMLKCHRMOKJODO',TO_DATE('10/03/1990','DD/MM/YYYY'),'2220710877','Apt. 119 610 Kyle Vista, Dorcasborough, AZ 07445',NULL);
 INSERT INTO person VALUES ('cyrus.hammes@example.com',ARRAY['Trainer']::roles[],'Cyrus','Hammes','d41d8cd98f00b204e9800998ecf8427e','KJADPAVW8KX5S0UQ',TO_DATE('22/02/1985','DD/MM/YYYY'),'0519398218','Apt. 168 747 Daniel Glen, East Keenan, OH 35920',NULL);
-INSERT INTO person VALUES ('bart.collins@example.com',ARRAY['Trainee']::roles[],'Bart','Collins','d41d8cd98f00b204e9800998ecf8427e','YOY6TKBDKLICSDBB',TO_DATE('20/11/1993','DD/MM/YYYY'),'9849796961','61356 Erin Harbor, Lake Sherman, IL 47716',NULL);
-INSERT INTO person VALUES ('nathanial.prohaska@example.com',ARRAY['Trainer']::roles[],'Nathanial','Prohaska','d41d8cd98f00b204e9800998ecf8427e','RE1BE14AWWYX2Y4L',TO_DATE('06/01/1961','DD/MM/YYYY'),'1396809182','Suite 023 30803 Nader Valleys, Sengerville, PA 07282-1307',NULL);
-INSERT INTO person VALUES ('cristi.schaden@example.com',ARRAY['Trainee']::roles[],'Cristi','Schaden','d41d8cd98f00b204e9800998ecf8427e','DI2RTBAM8YWBSI5R',TO_DATE('14/06/1993','DD/MM/YYYY'),'8696726751','0149 Goyette Ports, West Crystalhaven, MN 53493',NULL);
-INSERT INTO person VALUES ('shery.stamm@example.com',ARRAY['Trainer']::roles[],'Shery','Stamm','d41d8cd98f00b204e9800998ecf8427e','WTUZV4V1DE1UFFUI',TO_DATE('17/10/1970','DD/MM/YYYY'),'6161813924','86778 Bogisich Road, Zoeview, AK 87873-9690',NULL);
-INSERT INTO person VALUES ('alonzo.lindgren@example.com',ARRAY['Trainee']::roles[],'Alonzo','Lindgren','d41d8cd98f00b204e9800998ecf8427e','MQRCLW9GUXOEOCZE',TO_DATE('02/08/1987','DD/MM/YYYY'),'0537593185','3249 Tanna Motorway, West Marlena, AK 22530',NULL);
-INSERT INTO person VALUES ('dorris.trantow@example.com',ARRAY['Secretary']::roles[],'Dorris','Trantow','d41d8cd98f00b204e9800998ecf8427e','BQQWDHZRKXT9A5YI',TO_DATE('30/10/1983','DD/MM/YYYY'),'6740753156','9688 Pacocha Port, New Bryan, IN 26529',NULL);
-INSERT INTO person VALUES ('kraig.larkin@example.com',ARRAY['Secretary']::roles[],'Kraig','Larkin','d41d8cd98f00b204e9800998ecf8427e','T9XQHGBJ2NOYFJ0X',TO_DATE('31/12/1996','DD/MM/YYYY'),'0488874925','Apt. 444 157 Roob Bridge, South Earlean, CO 81294-2901',NULL);
-INSERT INTO person VALUES ('bao.glover@example.com',ARRAY['Trainee']::roles[],'Bao','Glover','d41d8cd98f00b204e9800998ecf8427e','W3CT5DQZUF3OIO2E',TO_DATE('03/12/1964','DD/MM/YYYY'),'6742914291','Suite 500 383 Myung Isle, West Hazelfort, KS 54896-0334',NULL);
 INSERT INTO person VALUES ('alfonso.gibson@example.com',ARRAY['Secretary']::roles[],'Alfonso','Gibson','d41d8cd98f00b204e9800998ecf8427e','RURTNQXG5SNSRLFC',TO_DATE('26/02/1973','DD/MM/YYYY'),'1559357297','5964 Lori Stravenue, Port Tyreeborough, AR 17660',NULL);
 INSERT INTO person VALUES ('carmine.sipes@example.com',ARRAY['Secretary']::roles[],'Carmine','Sipes','d41d8cd98f00b204e9800998ecf8427e','LCRKVACORBCT753D',TO_DATE('04/06/1997','DD/MM/YYYY'),'8151088748','99860 Lavonne Mall, New Lloyd, DE 43817-1522',NULL);
-INSERT INTO person VALUES ('altha.ruecker@example.com',ARRAY['Trainee']::roles[],'Altha','Ruecker','d41d8cd98f00b204e9800998ecf8427e','7IM7ZA79M0JKEDOE',TO_DATE('27/09/1959','DD/MM/YYYY'),'7216835609','34074 Luciano Loaf, Timton, IN 24432-9964',NULL);
+INSERT INTO person VALUES ('dorris.trantow@example.com',ARRAY['Secretary']::roles[],'Dorris','Trantow','d41d8cd98f00b204e9800998ecf8427e','BQQWDHZRKXT9A5YI',TO_DATE('30/10/1983','DD/MM/YYYY'),'6740753156','9688 Pacocha Port, New Bryan, IN 26529',NULL);
+INSERT INTO person VALUES ('kraig.larkin@example.com',ARRAY['Secretary']::roles[],'Kraig','Larkin','d41d8cd98f00b204e9800998ecf8427e','T9XQHGBJ2NOYFJ0X',TO_DATE('31/12/1996','DD/MM/YYYY'),'0488874925','Apt. 444 157 Roob Bridge, South Earlean, CO 81294-2901',NULL);
 
 --Medical certificate
 INSERT INTO medicalcertificate VALUES ('mario.rossi@example.com',TO_DATE('01/01/2023','DD/MM/YYYY'),'Doctor Sport', 'FAKE_PATH_FOR_FILE');
 INSERT INTO medicalcertificate VALUES ('riccardo.bianchi@example.com',TO_DATE('01/01/2023','DD/MM/YYYY'),'Doctor Sport', 'FAKE_PATH_FOR_FILE');
 INSERT INTO medicalcertificate VALUES ('luca.giallo@example.com',TO_DATE('01/01/2023','DD/MM/YYYY'),'Doctor Sport', 'FAKE_PATH_FOR_FILE');
+--Missing
 
 --Courses
 INSERT INTO course VALUES ('Powerlifting','Powerlifting is a strength sport that consists of three attempts at maximal weight on three lifts: squat, bench press, and deadlift. As in the sport of Olympic weightlifting, it involves the athlete attempting a maximal weight single-lift effort of a barbell loaded with weight plates.');
@@ -39,9 +34,21 @@ INSERT INTO course VALUES ('Crossfit','A form of high intensity interval trainin
 INSERT INTO course VALUES ('Bodybuilding','Bodybuilding, a regimen of exercises designed to enhance the human body''s muscular development and promote general health and fitness. As a competitive activity, bodybuilding aims to display in artistic fashion pronounced muscle mass, symmetry, and definition for overall aesthetic effect.');
 INSERT INTO course VALUES ('Calisthenics','Calisthenics is a form of strength training consisting of a variety of movements that exercise large muscle groups (gross motor movements), such as standing, grasping, pushing, etc. These exercises are often performed rhythmically and with minimal equipment, as bodyweight exercises.');
 
+--CourseEdition
 INSERT INTO courseedition VALUES (DEFAULT, 'Powerlifting')
+INSERT INTO courseedition VALUES (DEFAULT, 'Cardio')
+INSERT INTO courseedition VALUES (DEFAULT, 'Yoga')
+INSERT INTO courseedition VALUES (DEFAULT, 'Crossfit')
+INSERT INTO courseedition VALUES (DEFAULT, 'Bodybuilding')
+INSERT INTO courseedition VALUES (DEFAULT, 'Calisthenics')
 
+--Course
 INSERT INTO teaches VALUES (1,'Powerlifting','giacomo.forza@example.com');
+INSERT INTO teaches VALUES (2,'Cardio','nathanial.prohaska@example.com');
+INSERT INTO teaches VALUES (3,'Yoga','shery.stamm@example.com');
+INSERT INTO teaches VALUES (4,'Crossfit','nathanial.prohaska@example.com');
+INSERT INTO teaches VALUES (5,'Bodybuilding','cyrus.hammes@example.com');
+INSERT INTO teaches VALUES (6,'Calisthenics','annis.dach@example.com');
 
 
 --Room
