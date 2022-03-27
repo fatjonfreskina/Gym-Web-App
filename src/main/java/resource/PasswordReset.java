56e69edd0063472fa045ce3d7f57cde4acc8f76c
alemarco96 <alemarco96@libero.it>
2022-03-27 21:08:30 +0200

    AM - Added "Password Reset" bean.

---
+++ src/main/java/resource/PasswordReset.java
@@ -0,0 +1,47 @@
+package resource;
+
+import java.sql.Timestamp;
+
+public class PasswordReset
+{
+    private String token;
+    private Timestamp expirationDate;
+    private String person;
+
+    public PasswordReset(String token, Timestamp expirationDate, String person)
+    {
+        this.token = token;
+        this.expirationDate = expirationDate;
+        this.person = person;
+    }
+
+    public String getToken()
+    {
+        return token;
+    }
+
+    public Timestamp getExpirationDate()
+    {
+        return expirationDate;
+    }
+
+    public String getPerson()
+    {
+        return person;
+    }
+
+    public void setToken(String token)
+    {
+        this.token = token;
+    }
+
+    public void setExpirationDate(Timestamp expirationDate)
+    {
+        this.expirationDate = expirationDate;
+    }
+
+    public void setPerson(String person)
+    {
+        this.person = person;
+    }
+}
