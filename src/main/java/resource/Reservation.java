f7b782202a0ea3e2ec60cc81187b964347095377
Fatjon Freskina <fatjonfreskina@gmail.com>
2022-03-28 09:24:07 +0200

    Add Reservation.java

---
+++ src/main/java/resource/Reservation.java
@@ -0,0 +1,14 @@
+package resource;
+import java.time.LocalDate; //https://docs.oracle.com/javase/8/docs/api/java/time/LocalDate.html
+import java.time.LocalTime;
+
+
+public class Reservation {
+
+    private String trainee;
+    private String room;
+    private LocalDate lectureDate;
+    private 
+
+    
+}
