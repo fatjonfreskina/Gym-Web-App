7d582953a702754dc90b338f21901f77d8b23bd2
Francesco Caldivezzi <francesco.caldivezzi@gmail.com>
2022-03-29 19:17:12 +0200

    Added Bean Course

--- src/main/java/dao/InsertTeachesDatabase.java
+++ src/main/java/dao/InsertTeachesDatabase.java
@@ -8,43 +8,6 @@ import java.sql.SQLException;
 
 public class InsertTeachesDatabase {
 
-    private static final String STATEMENT = "INSERT INTO teaches (courseeditionid, coursename, trainer) VALUES (?, ?, ?)";
-    private final Connection con;
 
-    private final SubscriptionType subscriptionType;
-
-    /**
-     * Constructor for the InsertSubscriptionTypeDatabase class
-     *
-     * @param con  the connection to the database
-     * @param subscriptionType  the type of subscription to be added into the database
-     */
-    public InsertTeachesDatabase(final Connection con, final int courseEditionId, final Course)
-    {
-        this.con = con;
-        this.subscriptionType=subscriptionType;
-    }
-
-    /**
-     * Execute an Insert query to insert the provided SubscriptionType into the subscriptiontype table
-     */
-    public void insertSubscriptionType() throws SQLException
-    {
-        PreparedStatement preparedStatement = null;
-        try{
-            preparedStatement=con.prepareStatement(STATEMENT);
-            preparedStatement.setInt(1, subscriptionType.getCourseEditionID());
-            preparedStatement.setString(2, subscriptionType.getCourseName());
-            preparedStatement.setInt(3, subscriptionType.getDuration());
-            preparedStatement.setFloat(4, subscriptionType.getCost());
-        }
-        finally {
-            if(preparedStatement!=null){
-                preparedStatement.close();
-            }
-            con.close();
-        }
-
-    }
 
 }
--- src/main/java/resource/Course.java
+++ src/main/java/resource/Course.java
@@ -2,5 +2,17 @@ package resource;
 
 public class Course {
 
+    private final String name;
+    private final String description;
+
+    public Course(String name, String description)
+    {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName(){ return name; }
+
+    public String getDescription() { return description; }
     
 }
