cadec8b68a40975e2c5d485e65b405518847d2b3
Riccardo Forzan <riccardo.forzan@studenti.unipd.it>
2022-04-01 09:33:10 +0200

    UpdateUserSubscriptionDatabase created

--- src/main/java/dao/InsertUserSubscriptionDatabase.java
+++ src/main/java/dao/InsertUserSubscriptionDatabase.java
@@ -1,17 +1,14 @@
 package dao;
 
-import resource.Person;
 import resource.Subscription;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Inserts a new subscription in the database
  * @author Riccardo Forzan
  */
-public class InsertUserSubscription {
+public class InsertUserSubscriptionDatabase {
 
     private static final String STATEMENT =
             "INSERT INTO subscription (courseeditionid, coursename, duration, startday, discount, trainee) VALUES (?,?,?,?,?,?);";
@@ -23,32 +20,27 @@ public class InsertUserSubscription {
      * @param con JDBC database connection
      * @param subscription instance of Subscription {@link resource.Subscription} to add in the database
      */
-    public InsertUserSubscription(Connection con, Subscription subscription) {
+    public InsertUserSubscriptionDatabase(Connection con, Subscription subscription) {
         this.con = con;
         this.subscription = subscription;
     }
 
     /**
      * Inserts this object into the database
-     * @throws SQLException
+     * @throws SQLException if a database access error occurs
      */
     public void insert() throws SQLException {
-        PreparedStatement preparedStatement = null;
 
-        try {
-            preparedStatement = con.prepareStatement(STATEMENT);
+        try (PreparedStatement preparedStatement = con.prepareStatement(STATEMENT)) {
             preparedStatement.setInt(1, subscription.getCourseEditionID());
             preparedStatement.setString(2, subscription.getCourseName());
             preparedStatement.setInt(3, subscription.getDuration());
-            preparedStatement.setDate(4,subscription.getStartDay());
+            preparedStatement.setDate(4, subscription.getStartDay());
             preparedStatement.setInt(5, subscription.getDiscount());
             preparedStatement.setString(6, subscription.getTrainee());
             preparedStatement.execute();
 
-        }
-        finally {
-            if (preparedStatement != null)
-                preparedStatement.close();
+        } finally {
             con.close();
         }
 
---
+++ src/main/java/dao/UpdateUserSubscriptionDatabase.java
@@ -0,0 +1,51 @@
+package dao;
+
+import resource.Subscription;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Updates a subscription in the database
+ * @author Riccardo Forzan
+ */
+public class UpdateUserSubscriptionDatabase {
+
+    private static final String STATEMENT =
+            "UPDATE subscription SET startday = ?, discount = ?, trainee = ? WHERE courseeditionid = ?, coursename = ?, duration = ?;";
+    private final Connection con;
+    private final Subscription subscription;
+
+    /**
+     * Parametric constructor for the class
+     * @param con JDBC database connection
+     * @param subscription instance of Subscription {@link Subscription} to update in the database
+     */
+    public UpdateUserSubscriptionDatabase(Connection con, Subscription subscription) {
+        this.con = con;
+        this.subscription = subscription;
+    }
+
+    /**
+     * Updates a subscription object in the database
+     * @throws SQLException if a database access error occurs
+     */
+    public void update() throws SQLException {
+
+        try (PreparedStatement preparedStatement = con.prepareStatement(STATEMENT)) {
+            preparedStatement.setDate(1, subscription.getStartDay());
+            preparedStatement.setInt(2, subscription.getDiscount());
+            preparedStatement.setString(3, subscription.getTrainee());
+            preparedStatement.setInt(4, subscription.getCourseEditionID());
+            preparedStatement.setString(5, subscription.getCourseName());
+            preparedStatement.setInt(6, subscription.getDuration());
+            preparedStatement.execute();
+
+        } finally {
+            con.close();
+        }
+
+    }
+
+}
