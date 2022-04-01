67ab1213245e084936239865a4ac2d59b81cb827
Harjot Singh <harjot.98.singh@gmail.com>
2022-04-01 23:04:08 +0200

    SH - dao.InsertLTSD - now returning inserted LTS

--- src/main/java/dao/InsertLectureTimeSlotDatabase.java
+++ src/main/java/dao/InsertLectureTimeSlotDatabase.java
@@ -7,7 +7,10 @@ import resource.LectureTimeSlot;
 import java.sql.*;
 
 public class InsertLectureTimeSlotDatabase {
-  private final String STATEMENT = "INSERT INTO lecturetimeslot(roomname, date, starttime, courseeditionid,coursename,substitution) VALUES (?, ?, ?, ?, ?, ?)";
+  private final String STATEMENT = "" +
+      "INSERT INTO lecturetimeslot(roomname, date, starttime, courseeditionid,coursename,substitution)" +
+      "VALUES (?, ?, ?, ?, ?, ?) " +
+      "RETURNING *;";
   private final LectureTimeSlot lts;
   private final Connection connection;
   private static final Logger logger = LogManager.getLogger("harjot_singh_appender");
@@ -17,8 +20,11 @@ public class InsertLectureTimeSlotDatabase {
     this.lts = lts;
   }
 
-  public void insertLectureTimeSlot() throws SQLException {
+  public LectureTimeSlot insertLectureTimeSlot() throws SQLException {
     PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    LectureTimeSlot insertedLTS = null;
     try {
       ps = connection.prepareStatement(STATEMENT);
       ps.setString(1, lts.getRoomName());
@@ -28,18 +34,30 @@ public class InsertLectureTimeSlotDatabase {
       ps.setString(5, lts.getCourseName());
       ps.setString(6, lts.getSubstitution());
 
-      ps.execute();
-      logger.debug("[DEBUG] dao.InsertLTSD - %s - %s Inserted successfully.\n".formatted(
-          new Timestamp(System.currentTimeMillis()), lts));
-    } catch (SQLException ex) {
-      logger.error("[ERROR] dao.InsertLTSD - %s - Exception during insertion.\n%s\n".
-          formatted(new Timestamp(System.currentTimeMillis()), ex.getMessage()));
+      rs = ps.executeQuery();
+      if(rs.next()) {
+        String roomName = rs.getString("roomname");
+        Date date = rs.getDate("date");
+        Time startTime = rs.getTime("starttime");
+        int courseEditionId = rs.getInt("courseeditionid");
+        String courseName = rs.getString("coursename");
+        String substitution = rs.getString("substitution");
+        insertedLTS = new LectureTimeSlot(roomName, date, startTime, courseEditionId, courseName, substitution);
 
+        logger.debug("[DEBUG] gwa.dao.InsertLTSD - %s - %s Inserted successfully.\n".formatted(
+            new Timestamp(System.currentTimeMillis()), insertedLTS));
+      }
+    } catch (SQLException ex) {
+      logger.error("[ERROR] gwa.dao.InsertLTSD - %s - Exception during insertion.\n%s\n".
+          formatted(new Timestamp(System.currentTimeMillis()), ex.getMessage()));
       throw ex;
     } finally {
+      if (rs != null)
+        rs.close();
       if (ps != null)
         ps.close();
       connection.close();
     }
+    return insertedLTS;
   }
 }
