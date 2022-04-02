e348b0175053d9862b299e3d82851ab8fba56362
Francesco Caldivezzi <francesco.caldivezzi@gmail.com>
2022-04-02 18:06:17 +0200

    Commented MailManager

--- src/main/java/utils/MailManager.java
+++ src/main/java/utils/MailManager.java
@@ -9,19 +9,28 @@ import jakarta.mail.internet.MimeMessage;
 import resource.Person;
 import java.util.Properties;
 
+/**
+ * This class is used to manage mail massages
+ * @author Francesco Caldivezzi
+ * */
 public class MailManager
 {
-    public static void sendMail(String to,String title ,String msg, final Person person)
+    /**
+     * This method is used to send a mail message given a title a message and the person to send it
+     * @param title : the title of the mail
+     * @param msg : the message to send
+     * @param person : the person to which we need to send the message
+     * */
+    public static void sendMail(String title ,String msg, final Person person)
     {
         Properties properties = System.getProperties();
         properties.setProperty(Constants.MAILSMTPHOST, Constants.HOST);
         Session session = Session.getDefaultInstance(properties);
-
         try
         {
             MimeMessage message = new MimeMessage(session);
             message.setFrom(new InternetAddress(Constants.NOREPLYEMAIL));
-            message.addRecipient(MimeMessage.RecipientType.TO, new InternetAddress(to));
+            message.addRecipient(MimeMessage.RecipientType.TO, new InternetAddress(person.getEmail()));
             message.setSubject(title);
             message.setText("Dear "+person.getName()+" "+person.getSurname()+",\n"+
                     "\n\n" +
