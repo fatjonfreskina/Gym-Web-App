7c9a550bf8dea2234046a12e04284232e8631020
Riccardo Forzan <riccardo.forzan@studenti.unipd.it>
2022-04-02 10:50:54 +0200

    Add InitListener

---
+++ src/main/java/jobs/InitListener.java
@@ -0,0 +1,27 @@
+package jobs;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * This class is called when the application is initialized.
+ * It launches the jobs to be executed.
+ * @author Riccardo Forzan
+ */
+public class InitListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+
+        //Create a scheduler
+        var scheduler = Executors.newSingleThreadScheduledExecutor();
+
+        //Super call
+        jakarta.servlet.ServletContextListener.super.contextInitialized(sce);
+
+    }
+
+}
