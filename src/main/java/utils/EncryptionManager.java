83154aa85c0792ffee5e0dce5a7ba714bb4a57a1
Francesco Caldivezzi <francesco.caldivezzi@gmail.com>
2022-04-03 21:54:47 +0200

    EncriptionManager

---
+++ src/main/java/utils/EncryptionManager.java
@@ -0,0 +1,16 @@
+package utils;
+
+import jakarta.xml.bind.DatatypeConverter;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class EncryptionManager
+{
+    private static final String TYPE_ENCRIPTION = "SHA-256";
+    public static String encrypt(String string) throws NoSuchAlgorithmException
+    {
+        return DatatypeConverter.printHexBinary(MessageDigest.getInstance(TYPE_ENCRIPTION).digest(string.getBytes(StandardCharsets.UTF_8)));
+    }
+}
