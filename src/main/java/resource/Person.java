15ff7a7280db0238fd3a4ceec8d8112264a14278
Riccardo Forzan <riccardo.forzan@studenti.unipd.it>
2022-04-04 07:59:01 +0200

    Started the refactoring of Person

--- src/main/java/resource/Person.java
+++ src/main/java/resource/Person.java
@@ -6,101 +6,65 @@ import java.sql.Date;
 
 public class Person {
 
-    public static final String[] roles = {"Trainee","Trainer", "Secretary"};
-
-    public static final String ROLE_TRAINEE = "Trainee";
-    public static final String ROLE_TRAINER = "Trainer";
-    public static final String ROLE_SECRETARY = "Secretary";
     public static final int LENGTH_TELEPHONE = 10;
     public static final int MIN_AGE = 14;
 
-    private final Integer[] role;
     private final String email;
-    private final String pathImg;
-    private final String password;
-    private final String address;
     private final String name;
     private final String surname;
-    private final String taxCode;
-    private final Date birthDate;
+    private final String psw;
+    private final String taxcode;
+    private final Date birthdate;
     private final String telephone;
+    private final String address;
+    private final String avatarpath;
 
-    //Constructor with all the attributes
-    public Person(Integer[] role, String email, String pathImg, String password, String address, String name, String surname, String taxCode, Date birthDate, String telephone) {
-        this.role = role;
+    public Person(String email, String name, String surname, String psw, String taxcode, Date birthdate, String telephone, String address, String avatarpath) {
         this.email = email;
-        this.pathImg = pathImg;
-        this.password = password;
-        this.address = address;
         this.name = name;
         this.surname = surname;
-        this.taxCode = taxCode;
-        this.birthDate = birthDate;
+        this.psw = psw;
+        this.taxcode = taxcode;
+        this.birthdate = birthdate;
         this.telephone = telephone;
+        this.address = address;
+        this.avatarpath = avatarpath;
     }
 
-    //Constructor with no Path_img
-    public Person(Integer[] role, String email, String password, String address, String name, String surname, String taxCode, Date birthDate, String telephone) {
-        this(role, email, "", password, address, name, surname, taxCode, birthDate, telephone);
-    }
-    //Constructor for either only email or taxCode
-    public Person(String string, String constant)
-    {
-        this.role = null;
-        this.address = null;
-        this.birthDate = null;
-        this.password = null;
-        this.surname = null;
-        this.pathImg = null;
-        this.name = null;
-        this.telephone = null;
-        if(constant.equals(Constants.EMAIL))
-        {
-            this.email = string;
-            this.taxCode = null;
-        }else
-        {
-            this.taxCode = string;
-            this.email = null;
-        }
-    }
-
-    public final Date getBirthDate() {
-        return birthDate;
-    }
-
-    public final String getTaxCode() {
-        return taxCode;
-    }
-
-    public final String getSurname() {
-        return surname;
-    }
-
-    public final String getName() {
-        return name;
-    }
-
-    public final String getAddress() {
-        return address;
-    }
-
-    public final String getPassword() {
-        return password;
-    }
-
-    public final String getPathImg() {
-        return pathImg;
-    }
-
-    public final String getEmail() {
+    public String getEmail() {
         return email;
     }
 
-    public final Integer[] getRole() {
-        return role;
+    public String getName() {
+        return name;
     }
 
-    public final String getTelephone() { return telephone; }
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPsw() {
+        return psw;
+    }
+
+    public String getTaxcode() {
+        return taxcode;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getAvatarpath() {
+        return avatarpath;
+    }
 
 }
