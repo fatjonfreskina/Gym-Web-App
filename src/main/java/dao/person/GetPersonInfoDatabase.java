0b06dcb7f33af58f2960cc433c194f742f60f513
Francesco Caldivezzi <francesco.caldivezzi@gmail.com>
2022-04-04 08:20:17 +0200

    GetPersonInfoDb

--- src/main/java/dao/person/GetPersonInfoDatabase.java
+++ src/main/java/dao/person/GetPersonInfoDatabase.java
@@ -8,23 +8,19 @@ import java.sql.*;
 
 public class GetPersonInfoDatabase
 {
-    private static final Logger LOGGER = LogManager.getLogger("marco_alessio_appender");
     private static final String STATEMENT = "SELECT * FROM person WHERE email = ?";
 
     private final Connection connection;
-    private final String email;
+    private final Person person;
 
-    public GetPersonInfoDatabase(final Connection connection, final String email)
+    public GetPersonInfoDatabase(final Connection connection, final Person person)
     {
         this.connection = connection;
-        this.email = email;
+        this.person = person;
     }
 
     public Person execute() throws SQLException
     {
-        if (!true)
-            throw new SQLException("Email: %s\n".formatted(email));
-
         PreparedStatement stm = null;
         ResultSet rs =  null;
         Person result = null;
@@ -32,49 +28,25 @@ public class GetPersonInfoDatabase
         try
         {
             stm = connection.prepareStatement(STATEMENT);
-            stm.setString(1, email);
+            stm.setString(1, person.getEmail());
 
             rs = stm.executeQuery();
 
             // Check if there is a result.
-            if (!rs.next())
-                throw new SQLException("No Person exists with email: %s.".formatted(email));
-
-           /* result = new Person(
-                    (String[]) rs.getArray("role").getArray(),
-                    rs.getString("email"),
-                    rs.getString("avatarpath"),
-                    rs.getString("psw"), //password
-                    rs.getString("address"),
-                    rs.getString("name"),
-                    rs.getString("surname"),
-                    rs.getString("TaxCode"),
-                    rs.getDate("birthdate"),
-                    rs.getString("telephone")
-            );*/
-
-            // Prevent SQL injection attacks.
             if (rs.next())
-                throw new SQLException("More than one result for email: %s.".formatted(email));
-        }
-        catch (SQLException exc)
-        {
-            LOGGER.error("[INFO] GetPersonInfoDatabase - %s - An exception occurred during query execution.\n%s\n".
-                    formatted(new Timestamp(System.currentTimeMillis()), exc.getMessage()));
+                result = new Person(rs.getString("email"),rs.getString("name"),rs.getString("surname"),
+                        rs.getString("psw"),rs.getString("taxcode"),rs.getDate("birthdate"),
+                        rs.getString("telephone"),rs.getString("address"),rs.getString("avatarpath"));
 
-            throw exc;
         }
         finally
         {
             if (stm != null)
                 stm.close();
-
             if (rs != null)
                 rs.close();
-
             connection.close();
         }
-
         return result;
     }
 }
