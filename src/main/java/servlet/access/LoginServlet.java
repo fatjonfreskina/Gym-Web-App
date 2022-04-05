d9ad38449618dd80a385d4a3d08d4fef01e96e2e
Francesco Caldivezzi <francesco.caldivezzi@gmail.com>
2022-04-05 20:41:13 +0200

    Added LoginServlet

--- src/main/java/servlet/access/LoginServlet.java
+++ src/main/java/servlet/access/LoginServlet.java
@@ -1,6 +1,24 @@
 package servlet.access;
 
+import constants.Constants;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import servlet.AbstractServlet;
 
+import java.io.IOException;
+
 public class LoginServlet extends AbstractServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
+    {
+        req.getRequestDispatcher(Constants.PATH_LOGIN).forward(req, res);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
+    {
+        //Here control input and then redirect to trainee, trainer or secretary page
+    }
 }
