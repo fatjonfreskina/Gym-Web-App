b33a1f099fc5e79e40cdafebcc73179ddb25a1e6
alemarco96 <alemarco96@libero.it>
2022-04-08 15:10:19 +0200

    AM - PersonalInfoServlet uses session now.

--- src/main/java/servlet/PersonalInfoServlet.java
+++ src/main/java/servlet/PersonalInfoServlet.java
@@ -5,6 +5,7 @@ import dao.person.GetPersonByEmailDatabase;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import resource.Person;
 
 import javax.naming.NamingException;
@@ -14,18 +15,32 @@ import java.sql.SQLException;
 /**
  * @author Marco Alessio
  */
-public class PersonalInfoServlet extends AbstractServlet {
+public class PersonalInfoServlet extends AbstractServlet
+{
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        try {
-            //TODO: how to know the email of the logged user?
-            final String email = req.getParameter("email");
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
+    {
+        //Old version way to get the "email" of the user.
+        //final String email = req.getParameter("email");
 
+        HttpSession session = req.getSession(false);
+        if (session == null)
+            res.sendRedirect(Constants.RELATIVE_URL_LOGIN);
+
+        final String email = (String) session.getAttribute("email");
+        //TODO: can this really happen? Anyway, find a more suitable action to perform.
+        if (email == null)
+            res.sendRedirect(Constants.RELATIVE_URL_LOGIN);
+
+        try
+        {
+            //TODO: is still needed to query from the database, or the data is already available through the session object?
             Person person = new GetPersonByEmailDatabase(getDataSource().getConnection(), email).execute();
 
             req.setAttribute("personalInfo", person);
             req.getRequestDispatcher(Constants.PATH_PERSONALINFO).forward(req, res);
-        } catch (NamingException | SQLException e) {
+        } catch (NamingException | SQLException e)
+        {
             throw new ServletException(e);
         }
     }
