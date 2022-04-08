0809bbf20642195733613be36b2afbb36e40a5b0
Harjot Singh <harjot.98.singh@gmail.com>
2022-04-08 17:16:45 +0200

    SH - Bug corrected on Contant naming, logs more readable

--- src/main/java/dao/lecturetimeslot/GetLectureTimeSlotsByCourseDatabase.java
+++ src/main/java/dao/lecturetimeslot/GetLectureTimeSlotsByCourseDatabase.java
@@ -13,43 +13,50 @@ import java.util.List;
  * @author Harjot Singh
  */
 public class GetLectureTimeSlotsByCourseDatabase {
-    private static final String STATEMENT = "SELECT * FROM lecturetimeslot WHERE courseEditionId = ? and courseName = ?";
-    private final Connection connection;
-    private final LectureTimeSlot lectureTimeSlot;
+  private static final String STATEMENT = "SELECT * FROM lecturetimeslot WHERE courseEditionId = ? and courseName = ?";
+  private final Connection connection;
+  private final LectureTimeSlot lectureTimeSlot;
+  private static final Logger logger = LogManager.getLogger("harjot_singh_logger");
 
-    public GetLectureTimeSlotsByCourseDatabase(final Connection connection, final LectureTimeSlot lectureTimeSlot) {
-        this.connection = connection;
-        this.lectureTimeSlot = lectureTimeSlot;
+  public GetLectureTimeSlotsByCourseDatabase(final Connection connection, final LectureTimeSlot lectureTimeSlot) {
+    this.connection = connection;
+    this.lectureTimeSlot = lectureTimeSlot;
+  }
+
+  public List<LectureTimeSlot> execute() throws SQLException {
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+    List<LectureTimeSlot> result = new ArrayList<>();
+    try {
+      ps = connection.prepareStatement(STATEMENT);
+      ps.setInt(1, lectureTimeSlot.getCourseEditionId());
+      ps.setString(2, lectureTimeSlot.getCourseName());
+
+      rs = ps.executeQuery();
+
+      logger.debug("gwa.dao.GetLTSByCourseD: query executed successfully, retrieving data...");
+      while (rs.next()) {
+        String roomName = rs.getString(Constants.LECTURETIMESLOT_ROOMNAME);
+        Date date = rs.getDate(Constants.LECTURETIMESLOT_DATE);
+        Time startTime = rs.getTime(Constants.LECTURETIMESLOT_STARTTIME);
+        int courseEditionId = rs.getInt(Constants.LECTURETIMESLOT_COURSEEDITIONID);
+        String courseName = rs.getString(Constants.LECTURETIMESLOT_COURSENAME);
+        String substitution = rs.getString(Constants.LECTURETIMESLOT_SUBSTITUTION);
+        LectureTimeSlot lts = new LectureTimeSlot(roomName, date, startTime, courseEditionId, courseName, substitution);
+
+        logger.debug("gwa.dao.GetLTSByCourseD: Retrieved " + lts);
+        result.add(lts);
+      }
+    } catch (SQLException ex) {
+      logger.error("gwa.dao.GetLTSByCourseD: " + ex.getMessage());
+      throw ex;
+    } finally {
+      if (rs != null)
+        rs.close();
+      if (ps != null)
+        ps.close();
+      connection.close();
     }
-
-    public List<LectureTimeSlot> execute() throws SQLException {
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        List<LectureTimeSlot> result = new ArrayList<>();
-        try {
-            ps = connection.prepareStatement(STATEMENT);
-            ps.setInt(1, lectureTimeSlot.getCourseEditionId());
-            ps.setString(2, lectureTimeSlot.getCourseName());
-
-            rs = ps.executeQuery();
-
-            while (rs.next()) {
-                String roomName = rs.getString(Constants.LECTURETIMESLOT_ROOMNAME);
-                Date date = rs.getDate(Constants.LECTURETIMESLOT_DATE);
-                Time startTime = rs.getTime(Constants.LECTURETIMESLOT_STARTTIME);
-                int courseEditionId = rs.getInt(Constants.LECTURETIMESLOT_COURSEEDITIONID);
-                String courseName = rs.getString(Constants.LECTURETIMESLOT_COURSENAME);
-                String substitution = rs.getString(Constants.LECTURETIMESLOT_SUBSTITUITION);
-                LectureTimeSlot lts = new LectureTimeSlot(roomName, date, startTime, courseEditionId, courseName, substitution);
-                result.add(lts);
-            }
-        } finally {
-            if (rs != null)
-                rs.close();
-            if (ps != null)
-                ps.close();
-            connection.close();
-        }
-        return result;
-    }
+    return result;
+  }
 }
--- src/main/java/dao/lecturetimeslot/UpdateLectureTimeSlotDatabase.java
+++ src/main/java/dao/lecturetimeslot/UpdateLectureTimeSlotDatabase.java
@@ -11,68 +11,52 @@ import java.sql.*;
  * @author Harjot Singh
  */
 public class UpdateLectureTimeSlotDatabase {
-    private final String STATEMENT =
-            "UPDATE lecturetimeslot " +
-                    "SET courseeditionid = ?," +
-                    "coursename = ?," +
-                    "substitution = ?" +
-                    "WHERE roomname = ?" +
-                    "AND date = ?" +
-                    "AND starttime = ?" +
-                    "RETURNING *;";
-    private final LectureTimeSlot lts;
-    private final Connection connection;
-    private static final Logger logger = LogManager.getLogger("harjot_singh_appender");
+  private final String STATEMENT = "UPDATE lecturetimeslot " + "SET courseeditionid = ?," + "coursename = ?," + "substitution = ?" + "WHERE roomname = ?" + "AND date = ?" + "AND starttime = ?" + "RETURNING *;";
+  private final LectureTimeSlot lts;
+  private final Connection connection;
+  private static final Logger logger = LogManager.getLogger("harjot_singh_logger");
 
-    public UpdateLectureTimeSlotDatabase(final Connection connection, final LectureTimeSlot lts) {
-        this.connection = connection;
-        this.lts = lts;
+  public UpdateLectureTimeSlotDatabase(final Connection connection, final LectureTimeSlot lts) {
+    this.connection = connection;
+    this.lts = lts;
+  }
+
+  public LectureTimeSlot execute() throws SQLException {
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+    LectureTimeSlot updatedLts = null;
+    try {
+      ps = connection.prepareStatement(STATEMENT);
+      ps.setInt(1, lts.getCourseEditionId());
+      ps.setString(2, lts.getCourseName());
+      ps.setString(3, lts.getSubstitution());
+
+      ps.setString(4, lts.getRoomName());
+      ps.setDate(5, lts.getDate());
+      ps.setTime(6, lts.getStartTime());
+
+      rs = ps.executeQuery();
+      logger.debug("gwa.dao.UpdateLTSD: query executed successfully");
+      if (rs.next()) {
+        String roomName = rs.getString(Constants.LECTURETIMESLOT_ROOMNAME);
+        Date date = rs.getDate(Constants.LECTURETIMESLOT_DATE);
+        Time startTime = rs.getTime(Constants.LECTURETIMESLOT_STARTTIME);
+        int courseEditionId = rs.getInt(Constants.LECTURETIMESLOT_COURSEEDITIONID);
+        String courseName = rs.getString(Constants.LECTURETIMESLOT_COURSENAME);
+        String substitution = rs.getString(Constants.LECTURETIMESLOT_SUBSTITUTION);
+        updatedLts = new LectureTimeSlot(roomName, date, startTime, courseEditionId, courseName, substitution);
+
+        logger.debug("gwa.dao.UpdateLTSD: Updated Successfully %s.".formatted(updatedLts.toString()));
+      } else logger.debug("gwa.dao.UpdateLTSD: %s NOT FOUND.".formatted(lts.toString()));
+    } catch (SQLException ex) {
+      logger.error("gwa.dao.UpdateLTSD: %s.".formatted(ex.getMessage()));
+      throw ex;
+    } finally {
+      if (rs != null) rs.close();
+      if (ps != null) ps.close();
+      connection.close();
     }
 
-    public LectureTimeSlot execute() throws SQLException {
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        LectureTimeSlot updatedLts = null;
-        try {
-            ps = connection.prepareStatement(STATEMENT);
-            ps.setInt(1, lts.getCourseEditionId());
-            ps.setString(2, lts.getCourseName());
-            ps.setString(3, lts.getSubstitution());
-
-            ps.setString(4, lts.getRoomName());
-            ps.setDate(5, lts.getDate());
-            ps.setTime(6, lts.getStartTime());
-
-            rs = ps.executeQuery();
-            logger.debug("[DEBUG] gwa.dao.UpdateLTSD - %s - query executed successfully\n".formatted(
-                    new Timestamp(System.currentTimeMillis())));
-            if (rs.next()) {
-                String roomName = rs.getString(Constants.LECTURETIMESLOT_ROOMNAME);
-                Date date = rs.getDate(Constants.LECTURETIMESLOT_DATE);
-                Time startTime = rs.getTime(Constants.LECTURETIMESLOT_STARTTIME);
-                int courseEditionId = rs.getInt(Constants.LECTURETIMESLOT_COURSEEDITIONID);
-                String courseName = rs.getString(Constants.LECTURETIMESLOT_COURSENAME);
-                String substitution = rs.getString(Constants.LECTURETIMESLOT_SUBSTITUITION);
-                updatedLts = new LectureTimeSlot(roomName, date, startTime, courseEditionId, courseName, substitution);
-
-                logger.debug("[DEBUG] gwa.dao.UpdateLTSD - %s - Updated Successfully: %s.\n".formatted(
-                        new Timestamp(System.currentTimeMillis()), updatedLts.toString()));
-            } else
-                logger.debug("[DEBUG] gwa.dao.UpdateLTSD - %s - Following LectureTimeSlot not found: %s.\n".formatted(
-                        new Timestamp(System.currentTimeMillis()), lts.toString()));
-        } catch (SQLException ex) {
-            logger.error("[ERROR] gwa.dao.UpdateLTSD - %s - Exception during insertion.\n%s\n".
-                    formatted(new Timestamp(System.currentTimeMillis()), ex.getMessage()));
-
-            throw ex;
-        } finally {
-            if (rs != null)
-                rs.close();
-            if (ps != null)
-                ps.close();
-            connection.close();
-        }
-
-        return updatedLts;
-    }
+    return updatedLts;
+  }
 }
