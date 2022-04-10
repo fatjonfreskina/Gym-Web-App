f22a8685feb183f50118b6ae612dd5fa0203e835
RiccardoTumiati <riccardo178tumiati@gmail.com>
2022-04-10 12:03:51 +0200

    VAlid sub

--- src/main/java/dao/subscription/GetValidSubscriptionByTrainee.java
+++ src/main/java/dao/subscription/GetValidSubscriptionByTrainee.java
@@ -1,28 +1,37 @@
 package dao.subscription;
 
 import constants.Constants;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import resource.CourseEdition;
-import resource.Subscription;
+import resource.Person;
+import resource.Teaches;
+import resource.view.Trainer;
+import resource.view.ValidSubscription;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+    @author Tumiati Riccardo
+ */
 public class GetValidSubscriptionByTrainee {
-    private static final String STATEMENT = "SELECT * FROM subscription WHERE courseEditionId = ? and courseName = ?";
+    private static final String statement = "SELECT subscription.coursename, name, surname, startday+ (duration || ' day')::interval as Expiration" +
+            "FROM subscription JOIN teaches ON subscription.courseEditionId = teaches.courseEditionId and subscription.courseName = teaches.courseName"+
+            "JOIN person ON teaches.trainer = person.email"+
+            "WHERE trainee = ? and startday+ (duration || ' day')::interval>CURRENT_DATE";
     private final Connection conn;
     private final String trainee_email;
-    private static final Logger logger = LogManager.getLogger("riccardo_tumiati_logger");
-    private final String loggerClass = "gwa.dao.subscription.GetSByCourseD: ";
 
     public GetValidSubscriptionByTrainee(final Connection conn, final String email) {
         this.conn = conn;
         this.trainee_email = email;
     }
 
-    public void execute() throws SQLException {
+    public List<ValidSubscription> execute() throws SQLException {
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        List<ValidSubscription> l_subscription = new ArrayList<>();
+
         //TODO
+        return null;
     }
 }
--- src/main/java/resource/view/ValidSubscription.java
+++ src/main/java/resource/view/ValidSubscription.java
@@ -1,4 +1,25 @@
 package resource.view;
 
+import resource.Person;
+import resource.Subscription;
+
+import java.util.List;
+
 public class ValidSubscription {
+    private final Subscription sub;
+    private final List<Person> trainers;
+
+    public ValidSubscription(Subscription sub, List<Person> trainers){
+        this.sub = sub;
+        this.trainers = trainers;
+    }
+
+    public Subscription getSub(){
+        return sub;
+    }
+
+    public List<Person> trainers(){
+        return trainers;
+    }
+
 }
