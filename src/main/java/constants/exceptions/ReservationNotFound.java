c8425f2055b9505bc46aa0247d8f5a233e16cd62
riccardoforzan <riccardo.forzan@studenti.unipd.it>
2022-04-30 09:42:06 +0200

    Adding missing javadoc

--- src/main/java/constants/exceptions/ReservationNotFound.java
+++ src/main/java/constants/exceptions/ReservationNotFound.java
@@ -2,8 +2,16 @@ package constants.exceptions;
 
 import constants.Codes;
 
+/**
+ * Exception thrown when a reservation has not been found in the database
+ *
+ * @author Harjot Singh
+ */
 public class ReservationNotFound extends CustomException {
-  public ReservationNotFound() {
-    super(Codes.RESERVATION_NOT_FOUND);
-  }
+    /**
+     * Constructs a new instance for this exception
+     */
+    public ReservationNotFound() {
+        super(Codes.RESERVATION_NOT_FOUND);
+    }
 }
