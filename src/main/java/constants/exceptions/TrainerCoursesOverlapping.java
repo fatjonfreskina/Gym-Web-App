5dac871fbac1737963702b7a3ee3b06a19e0a8fa
riccardoforzan <riccardo.forzan@studenti.unipd.it>
2022-04-30 09:54:42 +0200

    Still to implement the Constants class

--- src/main/java/constants/exceptions/TrainerCoursesOverlapping.java
+++ src/main/java/constants/exceptions/TrainerCoursesOverlapping.java
@@ -2,8 +2,17 @@ package constants.exceptions;
 
 import constants.Codes;
 
+/**
+ * Exception thrown when a trainer has already a lesson associated to a timeslot, so is not possible to add another
+ * course
+ *
+ * @author Harjot Singh
+ */
 public class TrainerCoursesOverlapping extends CustomException {
-  public TrainerCoursesOverlapping() {
-    super(Codes.OVERLAPPING_COURSES);
-  }
+    /**
+     * Constructs a new instance for this exception
+     */
+    public TrainerCoursesOverlapping() {
+        super(Codes.OVERLAPPING_COURSES);
+    }
 }
--- src/main/java/constants/exceptions/WrongTimeFormat.java
+++ src/main/java/constants/exceptions/WrongTimeFormat.java
@@ -3,10 +3,15 @@ package constants.exceptions;
 import constants.Codes;
 
 /**
+ * Exception thrown when a parameter has a wrong time format
+ *
  * @author Harjot Singh
  */
 public class WrongTimeFormat extends CustomException {
-  public WrongTimeFormat() {
-    super(Codes.WRONG_TIME_FORMAT);
-  }
+    /**
+     * Constructs a new instance for this exception
+     */
+    public WrongTimeFormat() {
+        super(Codes.WRONG_TIME_FORMAT);
+    }
 }
