3be98a83f899347ec37700feaf262880aa8ac1fa
riccardoforzan <riccardo.forzan@studenti.unipd.it>
2022-05-05 21:28:45 +0200

    Javadoc on some dao of course edition

--- src/main/java/dao/courseedition/InsertCourseEditionDatabase.java
+++ src/main/java/dao/courseedition/InsertCourseEditionDatabase.java
@@ -2,47 +2,60 @@ package dao.courseedition;
 
 import constants.Constants;
 import resource.CourseEdition;
-import resource.Person;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
- * @author Francesco Caldivezzi
+ * Inserts a new course edition in the database
  *
- * */
+ * @author Francesco Caldivezzi
+ */
 public class InsertCourseEditionDatabase {
 
+    /**
+     * SQL statement to be executed
+     */
     private static final String STATEMENT = "INSERT INTO courseedition values(DEFAULT,?) returning *";
 
+    /**
+     * Connection to the database
+     */
     private final Connection conn;
+
+    /**
+     * CourseEdition object to add
+     */
     private final CourseEdition courseEdition;
 
-
+    /**
+     * Parametric constructor
+     * @param conn          connection to the database
+     * @param courseEdition object to add to the database
+     */
     public InsertCourseEditionDatabase(final Connection conn, CourseEdition courseEdition) {
         this.conn = conn;
         this.courseEdition = courseEdition;
     }
 
-    public Integer execute() throws SQLException
-    {
+    /**
+     * Executes the insert operation in the database for the specified object
+     *
+     * @return id ot the inserted course edition id
+     * @throws SQLException is thrown if something goes wrong while querying the database
+     */
+    public Integer execute() throws SQLException {
         Integer key = null;
-        try (PreparedStatement ps = conn.prepareStatement(STATEMENT))
-        {
-
+        try (PreparedStatement ps = conn.prepareStatement(STATEMENT)) {
             ps.setString(1, courseEdition.getCourseName());
-            ResultSet resultSet =  ps.executeQuery();
-            if(resultSet.next())
+            ResultSet resultSet = ps.executeQuery();
+            if (resultSet.next())
                 key = resultSet.getInt(Constants.COURSEEDITION_ID);
-
-            //ps.execute();
-        }catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.println(e);
-
-        }
-
-        finally
-        {
+        } finally {
             conn.close();
         }
         return key;
