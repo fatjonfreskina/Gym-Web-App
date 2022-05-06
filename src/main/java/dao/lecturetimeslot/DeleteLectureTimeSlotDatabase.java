95b5fd74c6f4d98fa74853e408ab725f03c472c3
Simone D'Antimo <simonedantimo@gmail.com>
2022-05-06 10:15:18 +0200

    Javadoc

--- src/main/java/dao/lecturetimeslot/DeleteLectureTimeSlotDatabase.java
+++ src/main/java/dao/lecturetimeslot/DeleteLectureTimeSlotDatabase.java
@@ -12,15 +12,32 @@ import java.sql.*;
  * @author Riccardo Forzan
  */
 public class DeleteLectureTimeSlotDatabase {
+    /**
+     * Query to execute on the database
+     */
     private static final String STATEMENT = "DELETE FROM lecturetimeslot WHERE roomname=? AND date=? AND starttime=?";
+    /**
+     * LectureTimeSlot object to remove from the database
+     */
     private final LectureTimeSlot lts;
+    /**
+     * Database connection
+     */
     private final Connection connection;
 
+    /**
+     * @param connection database connection
+     * @param lts Lecture time slot object to remove
+     */
     public DeleteLectureTimeSlotDatabase(final Connection connection, final LectureTimeSlot lts) {
         this.connection = connection;
         this.lts = lts;
     }
 
+    /**
+     * Executes the statement to remove the object from the database
+     * @throws SQLException
+     */
     public void execute() throws SQLException {
         try (PreparedStatement ps = connection.prepareStatement(STATEMENT)) {
             ps.setString(1, lts.getRoomName());
