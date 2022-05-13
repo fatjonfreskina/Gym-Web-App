90b9efa6f888c17718526ef182efbb64950fc538
albertocampeol <alberto.campeol@outlook.it>
2022-05-13 17:47:41 +0200

    label change fix in add-medical-certificate.js

--- src/main/webapp/js/secretary/add-medical-certificate.js
+++ src/main/webapp/js/secretary/add-medical-certificate.js
@@ -11,6 +11,8 @@ $(document).ready(function() {
     //handle file upload label change
     $(".custom-file-input").on("change", function() {
         var fileName = $(this).val().split("\\").pop();
+        if(fileName === "")
+            fileName = "Choose File"
         $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
     });
 
