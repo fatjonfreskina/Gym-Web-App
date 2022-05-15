cd082d1e73180fd294cc29e0963b0634d7b5aab9
albertocampeol <alberto.campeol@outlook.it>
2022-05-15 13:20:44 +0200

    manage_roles.js fix

--- src/main/webapp/js/secretary/manage_roles.js
+++ src/main/webapp/js/secretary/manage_roles.js
@@ -2,31 +2,17 @@ $(document).ready(function() {
     const form = $('#form')
     const buttonUpdate = $('#btn-update')
 
-    //error boxes
-    let alertBox = $("#alert-box")
-    let messageBody = $("#alert-message-body")
-
     buttonUpdate.click(function (e) {
-        //e.preventDefault();
         let isTrainee = document.getElementById("trainee").checked
         let isTrainer = document.getElementById("trainer").checked
         let isSecretary = document.getElementById("secretary").checked
         if (!isTrainer && !isTrainee && !isSecretary)
         {
-            showMessage("Please choose a role for the selected user")
+            showWarningMessage("Please choose a role for the selected user")
             e.preventDefault();
             return false;
         }
         form.submit()
     })
-
-    function showMessage(message) {
-        messageBody.empty()
-        messageBody.text(message)
-        alertBox.show()
-        alertBox.fadeTo(2000, 500).slideUp(500, function () {
-            $(this).slideUp(500);
-        });
-    }
 
 })
