d6192f0846fec3605a3bba8cd53c58b52faf4597
Harjot Singh <harjot.98.singh@gmail.com>
2022-05-21 10:55:32 +0200

    SH - Added comments

--- src/main/webapp/js/trainer/trainer-attendance.js
+++ src/main/webapp/js/trainer/trainer-attendance.js
@@ -1,6 +1,28 @@
 $(document).ready(function () {
+    //GLOBAL VARIABLES
+    /**
+     *
+     * THE context path for the url
+     * @type {string} contextPath value
+     */
     let contextPath = "/wa2122-gwa"
 
+    /**
+     * global list of reservations == present trainees
+     * @type {*[]} empty list
+     */
+    let reservations;
+    /**
+     * global list of subscriptions == trainees enrolled to the course but absent
+     * @type {*[]} empty list
+     */
+    let subscriptions = [];
+
+    /**
+     * GET main details of a given Trainee from the backend
+     * @param email of the trainee
+     * @returns async call: if success return the trainee object as json otherwise returns an error
+     */
     GET_Trainee = (email) => {
         //Perform the AJAX request to fill the calendar
         return $.ajax({
@@ -14,12 +36,15 @@ $(document).ready(function () {
             },
             error: (response) => {
                 //console.log(response);
-                if (!response.responseJSON.isError)
-                    showWarningMessage("Server error while retrieving data");
+                if (!response.responseJSON.isError) showWarningMessage("Server error while retrieving data");
             }
         });
     }
 
+    /**
+     * GET the reservations and the subscription to the currentLectureTimeSlot from the server and update the tables
+     * @returns async call: if success return the trainerAttendance object as json otherwise returns an error
+     */
     GET_TrainerAttendance = () => {
         //console.log("GET_TrainerAttendance");
         return $.ajax({
@@ -28,17 +53,26 @@ $(document).ready(function () {
             type: "GET",
             dataType: 'json',
             success: function (response) {
+                /**
+                 * Update the reservations table
+                 */
                 updateReservations(response);
+                /**
+                 * Update the subscriptions table
+                 */
                 updateSubscriptions(response);
                 return response;
             },
             error: function (response) {
-                if (!response.responseJSON.isError)
-                    showWarningMessage("Server error while retrieving data");
+                if (!response.responseJSON.isError) showWarningMessage("Server error while retrieving data");
             }
         });
     }
 
+    /**
+     * POST a subscription to the server, to mark a trainee as present
+     * @param email of the trainee
+     */
     POST_Subscription = (email) => {
         //console.log(subscriptions);
         //let subscription = subscriptions.find(elem => elem.trainee = email); not working properly
@@ -66,12 +100,15 @@ $(document).ready(function () {
             },
             error: function (response) {
                 //console.log(response);
-                if (!response.responseJSON.isError)
-                    showWarningMessage("Server error sending data");
+                if (!response.responseJSON.isError) showWarningMessage("Server error sending data");
             }
         });
     }
 
+    /**
+     * DELETE a reservation from the server, to mark a trainee as absent
+     * @param email of the trainee
+     */
     DELETE_Reservation = (email) => {
         let reservation = reservations.find(elem => elem.trainee = email);
         $.ajax({
@@ -87,18 +124,14 @@ $(document).ready(function () {
             },
             error: function (err) {
                 //console.log(response);
-                if (!err.responseJSON.isError)
-                    showWarningMessage("Server error sending data");
+                if (!err.responseJSON.isError) showWarningMessage("Server error sending data");
             }
         });
     }
 
-    let reservations;
-    let subscriptions = [];
-
     /**
-     *
-     * @param rows
+     * Clears the Reservations table and then updates the table with the given data
+     * @param rows to be added to the table
      */
     updateReservationsTable = (rows) => {
         //console.log(rows)
@@ -114,8 +147,9 @@ $(document).ready(function () {
     }
 
     /**
-     *
-     * @param attendances
+     * Extracts reservation from the attendances
+     * and for each trainee, who has booked, gets his personal information from the server
+     * @param attendances trainers attendances of the current lecturetimeslot
      */
     updateReservations = (attendances) => {
         //console.log("updateReservations");
@@ -125,6 +159,9 @@ $(document).ready(function () {
             reservationsAJAX.push(GET_Trainee(reservations[i].trainee))
         }
 
+        /**
+         * Retrieve all the trainees, who made a reservation, and add them to the reservation table
+         */
         $.when(reservationsAJAX).done((res) => {
             let results = [];
             //console.log("RES", res);
@@ -143,14 +180,13 @@ $(document).ready(function () {
             }); else updateReservationsTable(results)
         }).fail(function (response) {
             //console.log(response);
-            if (!response.responseJSON.isError)
-                showWarningMessage("Server error while updating reservation");
+            if (!response.responseJSON.isError) showWarningMessage("Server error while updating reservation");
         });
     }
 
     /**
-     *
-     * @param rows
+     * Clears the Subscriptions table and then updates the table with the given data
+     * @param rows to be added to the table
      */
     updateSubscriptionsTable = (rows) => {
         //console.log(rows)
@@ -167,8 +203,9 @@ $(document).ready(function () {
     }
 
     /**
-     *
-     * @param attendance
+     * Extracts subscriptions from the attendances
+     * and for each trainee, who is enrolled to the course, gets his personal information from the server
+     * @param attendance trainers attendances of the current lecturetimeslot
      */
     updateSubscriptions = (attendance) => {
         //console.log("updateSubscriptions");
@@ -185,6 +222,9 @@ $(document).ready(function () {
             subsriptionsAJAX.push(GET_Trainee(subscriptions[i].trainee))
         }
 
+        /**
+         * Retrieve all the trainees, who are enrolled to the course, and add them to the subscriptions table
+         */
         $.when(subsriptionsAJAX).done((res) => {
             let results = [];
             //console.log("SUBSCRIPTIONS", res);
@@ -203,12 +243,13 @@ $(document).ready(function () {
             }); else updateSubscriptionsTable(results)
         }).fail(function (response) {
             //console.log(response);
-            if (!response.responseJSON.isError)
-                showWarningMessage("Server error while updating reservation");
+            if (!response.responseJSON.isError) showWarningMessage("Server error while updating reservation");
         });
     }
 
-    // CALL
+    /**
+     * First call to retrieve all the data and populate the tables
+     */
     GET_TrainerAttendance();
 
     /**
